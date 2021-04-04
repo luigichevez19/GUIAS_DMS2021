19dc779db2b0be84cc1a9dbbea799bf7ac544f48
Luis Chévez <luigichevez@gmail.com>
2021-04-04 10:01:14 -0600

    Ingresando con correo y contra

---
+++ Guia07App/app/src/main/java/sv/edu/udb/guia07app/DashBoardActivity.java
@@ -0,0 +1,13 @@
+package sv.edu.udb.guia07app;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DashBoardActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+}
